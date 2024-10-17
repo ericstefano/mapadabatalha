1d8be3b80cd9f0b640aa00917f198719f0b9bc3a
ericstefano <ericstefano12@gmail.com>
2024-10-17 11:12:09 -0300

    chore: update playwright

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
-export const APP_NAME = 'Vitesse for Nuxt 3'
+export const APP_NAME = 'Mapa da Batalha'
 export const APP_DESCRIPTION = 'Vitesse for Nuxt 3'
 export const INSTAGRAM_BASE_URL = 'https://www.instagram.com'
